70f2d06de1652d9957f37a77e812054211f1d399
Juhana Lankinen <juhana.lankinen@csc.fi>
2025-08-12 08:55:24 +0300

    Update setup script

--- scripts/setup_new_project.sh
+++ scripts/setup_new_project.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -xe
+
 # Take on argument from user, replace "skeleton" with that
 if [ $# -ne 1 ]
 then
@@ -7,18 +9,59 @@ then
     exit 1
 fi
 
+export project_name=$1
+
+git clone https://github.com/KJLankinen/cmake_skeleton.git $project_name
+cd $project_name
+
 for file in $(git grep -r skeleton | awk -F: '{print $1}')
 do
-    sed -i "s/skeleton/$1/g" $file
+    sed -i "s/skeleton/$project_name/g" $file
 done
 
 for file in $(git grep -r SKELETON | awk -F: '{print $1}')
 do
-    sed -i "s/SKELETON/\U$1/g" $file
+    sed -i "s/SKELETON/\U$project_name/g" $file
 done
 
-mv include/skeleton include/$1
+mv include/skeleton include/$project_name
+mkdir build
 
-echo "# About" > README.md
-echo "" >> README.md
-echo "This is a project called $1" >> README.md
+# Need to use envsubst instead of cat:
+# We want the $project_name to expand
+# We don't want the bacticks ` to expand
+# Thus, quoting EOF but substituting $project_name with the env var
+envsubst > README.md << 'EOF'
+# About
+
+This is a project called $project_name.
+
+It has the following directory structure:
+- `apps` contains a simple binary that calls the library code
+- `docs` contains files for generating html documentation using doxygen & dot
+- `include/$project_name` contains the header files of this project
+- `scripts` contains some scripts
+- `src` contains the source code for the library
+- `tests` contains a simple test using Catch2 testing framework
+
+You can configure and build this project with
+
+```bash
+cmake -S . -B build
+cmake --build build -t all
+```
+
+To execute the tests and the sample binary, do
+```bash
+# Run tests
+ctest --test-dir build
+# Or cmake --build build --target test
+
+# Run a sample binary
+build/apps/name_of_project-bin
+```
+EOF
+
+rm -rf .git
+rm -rf .gitignore
+rm TODO.md

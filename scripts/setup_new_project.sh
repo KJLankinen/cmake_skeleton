26f77df4a0b1c14ed8a4e886011594ffc87cd660
Juhana Lankinen <juhana.lankinen@csc.fi>
2025-08-12 09:14:48 +0300

    Clone local repository

--- scripts/setup_new_project.sh
+++ scripts/setup_new_project.sh
@@ -2,6 +2,29 @@
 
 set -e
 
+# Function taken from stack overflow for finding the directory of this script
+get_script_dir()
+{
+    local SOURCE_PATH="${BASH_SOURCE[0]}"
+    local SYMLINK_DIR
+    local SCRIPT_DIR
+    # Resolve symlinks recursively
+    while [ -L "$SOURCE_PATH" ]; do
+        # Get symlink directory
+        SYMLINK_DIR="$( cd -P "$( dirname "$SOURCE_PATH" )" >/dev/null 2>&1 && pwd )"
+        # Resolve symlink target (relative or absolute)
+        SOURCE_PATH="$(readlink "$SOURCE_PATH")"
+        # Check if candidate path is relative or absolute
+        if [[ $SOURCE_PATH != /* ]]; then
+            # Candidate path is relative, resolve to full path
+            SOURCE_PATH=$SYMLINK_DIR/$SOURCE_PATH
+        fi
+    done
+    # Get final script directory path from fully resolved source path
+    SCRIPT_DIR="$(cd -P "$( dirname "$SOURCE_PATH" )" >/dev/null 2>&1 && pwd)"
+    echo "$SCRIPT_DIR"
+}
+
 # Take on argument from user, replace "skeleton" with that
 if [ $# -ne 1 ]
 then
@@ -11,7 +34,7 @@ fi
 
 export project_name=$1
 
-git clone https://github.com/KJLankinen/cmake_skeleton.git $project_name
+git clone $(get_script_dir) $project_name
 cd $project_name
 
 for file in $(git grep -r skeleton | awk -F: '{print $1}')

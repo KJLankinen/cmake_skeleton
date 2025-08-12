aa4e7290b6c5befd64f60f687ee78b166059c411
Juhana Lankinen <juhana.lankinen@csc.fi>
2025-08-12 12:41:31 +0300

    Rename variables

--- include/skeleton/version.hpp
+++ include/skeleton/version.hpp
@@ -3,12 +3,13 @@
 #include <string_view>
 
 namespace skeleton {
-    extern const uint32_t SKELETON_VERSION_MAJOR;
-    extern const uint32_t SKELETON_VERSION_MINOR;
-    extern const uint32_t SKELETON_VERSION_PATCH;
-    extern const uint32_t SKELETON_VERSION_TWEAK;
-    extern const std::string_view SKELETON_VERSION_STRING;
-    extern const std::string_view GIT_HASH;
+extern const uint32_t PROJECT_VERSION_MAJOR;
+extern const uint32_t PROJECT_VERSION_MINOR;
+extern const uint32_t PROJECT_VERSION_PATCH;
+extern const uint32_t PROJECT_VERSION_TWEAK;
+extern const std::string_view PROJECT_VERSION_STRING;
+extern const std::string_view PROJECT_GIT_HASH;
+extern const std::string_view PROJECT_NAME;
 
-    void print_version();
+void print_version();
 }
